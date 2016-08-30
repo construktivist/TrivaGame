49b696a17c163a476d8f9d13b54ff2a33d9f2ee6
Keegan Kelly <keegankelly1@gmail.com>
2016-08-29 22:46:57 -0500

    Added logic for start button and displaying question/answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+
+
+	var $startButton = $('.button');
+	var $question = $('#question');
+	var $answer1 = $('#answer1');
+	var $answer2 = $('#answer2');
+	var $answer3 = $('#answer3');
+	var $answer4 = $('#answer4');
+
+	$($startButton).hover(function(){
+		$($startButton).toggleClass("button-hover");
+	});
+
+	$($startButton).click(function(){
+		$(this).hide();
+		console.log("Click works");
+
+		var trivia = {
+			question1: function(){
+				console.log("Trivia object works");
+				$($question).html("This is a sample for question1?");
+				$($answer1).html("This is answer1");
+				$($answer2).html("This is answer 2");
+				$($answer3).html("This is answer 3");
+				$($answer4).html("This is answer 4");
+			},
+
+			question2: function(){
+				console.log("Trivia object works");
+				$($question).html("This is a sample for question2?");
+				$($answer1).html("This is answer1");
+				$($answer2).html("This is answer 2");
+				$($answer3).html("This is answer 3");
+				$($answer4).html("This is answer 4");
+			},
+
+			question3: function(){
+				console.log("Trivia object works");
+				$($question).html("This is a sample for question3?");
+				$($answer1).html("This is answer1");
+				$($answer2).html("This is answer 2");
+				$($answer3).html("This is answer 3");
+				$($answer4).html("This is answer 4");
+			},
+
+			question4: function(){
+				console.log("Trivia object works");
+				$($question).html("This is a sample for question4?");
+				$($answer1).html("This is answer1");
+				$($answer2).html("This is answer 2");
+				$($answer3).html("This is answer 3");
+				$($answer4).html("This is answer 4");
+			},
+
+			question5: function(){
+				console.log("Trivia object works");
+				$($question).html("This is a sample for question5?");
+				$($answer1).html("This is answer1");
+				$($answer2).html("This is answer 2");
+				$($answer3).html("This is answer 3");
+				$($answer4).html("This is answer 4");
+			},
+		};
+
+
+
+
+
+		
+
+
+
+
+
+	});
+
+});
